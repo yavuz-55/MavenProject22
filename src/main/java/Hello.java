ba3d5c48be190218d7f87421de8455ed800ad3e5
yavuz-55 <yavuzyardim@gmail.com>
2022-05-11 11:45:48 +0100

    commit after setting github file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("developing Hello feature");
+        System.out.println("github Repo  is Already Published to GitHub");
+        System.out.println("doing more work");
+
     }
 }
